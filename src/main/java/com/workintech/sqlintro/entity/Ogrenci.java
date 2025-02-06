390c69656cacd1289e2213d8eb48652a72340e3d
Ali Umur Kucur <aliumurkucur@gmail.com>
2025-02-06 17:35:40 +0300

    All tests passed.

--- src/main/java/com/workintech/sqlintro/entity/Ogrenci.java
+++ src/main/java/com/workintech/sqlintro/entity/Ogrenci.java
@@ -4,11 +4,14 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 @Entity
+@Component
 @Table(name = "ogrenci")
 public class Ogrenci {
     @Id
